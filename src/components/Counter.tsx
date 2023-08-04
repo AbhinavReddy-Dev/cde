81a5967937c796516b88598302c1ebb9bec09d0a
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-08-04 16:16:19 -0400

    added SDG logos

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,25 +1,11 @@
-import { useState } from 'react';
-import './Counter.css';
+import { useState } from "react";
+import "./Counter.css";
+import React from "react";
 
-export default function Counter({
-	children,
-	count: initialCount,
-}: {
-	children: JSX.Element;
-	count: number;
-}) {
-	const [count, setCount] = useState(initialCount);
-	const add = () => setCount((i) => i + 1);
-	const subtract = () => setCount((i) => i - 1);
-
-	return (
-		<>
-			<div className="counter">
-				<button onClick={subtract}>-</button>
-				<pre>{count}</pre>
-				<button onClick={add}>+</button>
-			</div>
-			<div className="counter-message">{children}</div>
-		</>
-	);
+export default function Counter({ children }: { children: JSX.Element }) {
+  return (
+    <>
+      <text>CDE</text>
+    </>
+  );
 }
