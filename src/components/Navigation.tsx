76647bc9dd61592655eb1d26dc6e14ad38949d27
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-08-06 00:35:30 -0400

    nav margin fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav className="z-40 flex flex-col gap-2 px-2 py-3 md:px-5 w-full mx-auto md:my-6 sticky top-0  border border-gray-100 bg-white shadow-sm rounded-lg">
+    <nav className="z-40 flex flex-col gap-2 px-2 py-3 md:px-5 w-full mx-auto lg:my-6 sticky top-0  border border-gray-100 bg-white shadow-sm rounded-lg">
       <div className="flex justify-between py-2 text-blue-700 border-b border-gray-200">
         <p>Logo</p>
         <img
